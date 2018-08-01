56315974e337380fb73a74e313fc89cc99761e31
11 <1191273496@qq.com>
2018-08-01 17:10:26 +0800

    修改反向代理的错误[C

--- src/components/common/looplist.vue
+++ src/components/common/looplist.vue
@@ -7,8 +7,19 @@
 </template>
 
 <script>
+import axios from "axios"
 export default {
-	
+	data(){
+		return {
+			looplist:[]
+		}
+	},
+
+	mounted(){
+		axios.get('/Service/callback.mi/Showtime/ShowtimeMovieAndDateListByCinema.api?cinemaId=8878&t=20188116581065660').then(res=>{
+			console.log(res.data)
+		})
+	}
 
 }
 
