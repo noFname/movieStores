6b560c8a7b16fe7389d5295a87c00a1824bbb7c1
e <707229047@qq.com>
2018-08-01 18:15:18 +0800

    a

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,18 +1,29 @@
 <template>
 <div>
-	<h1><img src="" alt=""></h1>
+	<h1><img src=""></h1>
+	<mt-button class="btn">登陆</mt-button>
 </div>
 
 </template>
 
 <script>
+	import { Button } from 'mint-ui';
 export default {
-
+	components :{
+		"mt-button" : Button
+	}
 }
 
 
 </script>
 
-<style>
-
+<style scoped>
+	h1{
+		width: 100%;
+		background: #333;
+	}
+	.btn{
+		width: 50%;
+		margin-left: 25%;
+	}
 </style>
