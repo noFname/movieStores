7bc7bd3f6a9f0b84d3a59f6fe3e8e1624210369e
e <707229047@qq.com>
2018-08-02 11:04:38 +0800

    nothing

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,7 +1,23 @@
 <template>
 <div>
-	<h1><img src=""></h1>
-	<mt-button class="btn">登陆</mt-button>
+	<h1>
+		<span @click="handleClick()"><</span>
+		<a href="#"><img src="/static/logo_mtime.png" alt=""></a>
+	</h1>
+	<div class="inner">
+		<i class="iconfont">&#xe637;</i><input type="text" placeholder="登陆邮箱/手机号码" class="text"><br>
+		<i class="iconfont">&#xe64e;</i><input type="text" placeholder="密码">
+	</div>
+	
+	<mt-button type="primary" class="btn">登陆</mt-button><br>
+	<div class="other">
+		<a href="/#/reg" class="free">免费注册</a>
+		<a href="#" class="find">找回密码</a>
+	</div>
+	<p>使用第三方登录</p>
+	<a href="#" class="wb"></a>
+	<a href="#" class="qq"></a>
+	
 </div>
 
 </template>
@@ -9,6 +25,11 @@
 <script>
 	import { Button } from 'mint-ui';
 export default {
+	methods : {
+		handleClick(){
+			location.href="/#/index"
+		}
+	},
 	components :{
 		"mt-button" : Button
 	}
@@ -20,10 +41,96 @@ export default {
 <style scoped>
 	h1{
 		width: 100%;
+		height: 50px;
 		background: #333;
+		overflow: hidden;
+	}
+	h1 span{
+		float: left;
+		color: #fff
+	}
+	img{
+		height: 30px;
+		margin-left:45%;
+		transform: translateX(-50%);
+		margin-top: 10px;
+		float: left;
+
 	}
 	.btn{
-		width: 50%;
-		margin-left: 25%;
+		width: 90%;
+		margin-left: 5%;
+		border-radius:170px;
+		margin-top: 30px;
+		background: #1e7dd7
+	}
+	input{
+		width: 85%;
+		border: 0;
+		color: #666;
+		height: 50px;
+	}
+	.text{
+		border-bottom: 1px solid #ccc;
+		line-height: 50px
+	}
+	.free{
+		color: #1e7dd7;
+		font-weight: bold;
+		font-size: 14px;
+		float: left;
+		margin-left: 20px;
+		margin-top: 20px;
+	}
+	.find{
+		font-size: 14px;
+		float: right;
+		margin-right: 20px;
+		margin-top: 20px;
+	}
+	.other{
+		width: 345px;
+		height: 60px;
+		border-bottom:1px solid #ccc ;
+		margin-left: 15px;
+	}
+	p{
+		width: 100px;
+		font-size: 12px;
+		color: #999;
+		position: relative;
+		text-align: center;
+		top: -9px;
+		left: 135px;
+		background: #f0efed;
+	}
+	.wb{
+		display: block;
+		width: 52px;
+		height: 52px;
+		background: url(/static/o_sina.png) no-repeat center;
+		float: left;
+		background-size: cover;
+		margin-right: 30px;
+		margin-left: 120px;
+		margin-top: 10px;
+
+	}
+	.qq{
+		display: block;
+		width: 52px;
+		height: 52px;
+		background: url(/static/o_qq.png) no-repeat center;
+		float: left;
+		background-size: cover;
+		margin-top: 10px;
+	}
+	.iconfont{
+		font-size: 22px;
+		padding: 0 17px;
+	}
+	.inner{
+		width: 100%;
+		background: #fff
 	}
 </style>
--- src/components/reg.vue
+++ src/components/reg.vue
@@ -1,12 +1,49 @@
 <template>
 <div>
-	<h1>reg</h1>
-</div>
+	<h1>
+		<span @click="handleClick()"><</span>
+		<a href="#"><img src="/static/logo_mtime.png" alt=""></a>
+	</h1>
+	<div class="inner">
+		<input type="text" placeholder="手机号：请输入手机号" class="text"><br>
+		<input type="text" placeholder="密码：6-20位数字、字母、符号组成" >
+	</div>
+	
+	<p>
+		<mt-checklist
+ 			v-model="value"
+ 			:options="['我已阅读并同意《Mtime时光网服务条款》']">
+		</mt-checklist>
 
+	</p>
+	<mt-button type="primary" class="btn">注册</mt-button><br>
+
+
+</div>
 </template>
 
 <script>
+	import { Button } from 'mint-ui';
+	import { Checklist } from 'mint-ui';
+  	
 export default {
+
+	data(){
+
+		return {
+			value:['我已阅读并同意《Mtime时光网服务条款》']
+			
+		}
+	},
+	methods : {
+		handleClick(){
+			location.href="/#/login"
+		}
+	},
+	components :{
+		"mt-button" : Button,
+		"mt-checklist":Checklist
+	}
 
 }
 
@@ -14,5 +51,45 @@ export default {
 </script>
 
 <style scoped>
+	h1{
+		width: 100%;
+		height: 50px;
+		background: #333;
+		overflow: hidden;
+	}
+	h1 span{
+		float: left;
+		color: #fff
+	}
+	img{
+		height: 30px;
+		margin-left:45%;
+		transform: translateX(-50%);
+		margin-top: 10px;
+		float: left;
 
+	}
+	.btn{
+		width: 90%;
+		margin-left: 5%;
+		border-radius:170px;
+		background: #1e7dd7
+	}
+	input{
+		width: 95%;
+		border: 0;
+		color: #666;
+		height: 50px;
+		margin-left: 20px;
+	}
+	.text{
+		border-bottom: 1px solid #ccc;
+		line-height: 50px
+	}
+
+	.inner{
+		width: 100%;
+		background: #fff;
+		border-bottom: 1px solid #ccc;
+	}
 </style>
