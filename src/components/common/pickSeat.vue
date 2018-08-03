a9e003077bde8c28af5ab148e4bd1cd6bd897306
11 <1191273496@qq.com>
2018-08-03 13:29:21 +0800

    座位选择

--- src/components/common/pickSeat.vue
+++ src/components/common/pickSeat.vue
@@ -3,7 +3,7 @@
 <div>
 	<h1>pickseat</h1>
 	<div class="header">
-		<button class="back"> < </button>
+		<div class="back"> < </div>
 		<div class="hea">
 			<h3>西虹市首富</h3>
 			<p>2D中文</p>
@@ -14,7 +14,7 @@
 	<div class="seat">
 		<h3>剩余n个座</h3>
 		<ul>
-			<li v-for="data,index in seatlist">{{index}}</li>
+			<li v-for="data,index in seatlist" @click="handleClick(index)" :class="isShow?'pick':'' ">{{index}}</li>
 		</ul>
 	</div>
 
@@ -27,7 +27,20 @@
 export default {
 	data(){
 		return{
-			seatlist:[]
+			seatlist:[
+			{index,}
+			],
+			 isShow:false,
+			 currentIndex:0,
+			
+		}
+	},
+
+	methods:{
+		handleClick(index){
+			console.log("aaaaaa")
+
+			this.isShow = !this.isShow
 		}
 	}
 }
@@ -42,14 +55,43 @@ export default {
 		display: flex;
 		justify-content: space-between;
 		border-bottom: 1px solid #ccc;
+		color: #eee;
 		.null{
 			width: 30%;
 		}
 		.back{
 			width: 30%;
+			font-size: 40px;
 		}
 		.hea{
 			flex: 1;
+			text-align: center;
 		}
+	}
+	.seat {
+		h3{
+			width: 100%;
+			text-align: center;
+			padding: 10px 0;
+		}
+
+		ul{
+			padding :0 10%;	
+			width: 80%;			
+			display: flex;
+			justify-content: space-between;
+			background: pink;
+			flex-wrap: wrap;
+			margin-left: 10%;
+			li{
+				width: 15px;
+				height: 15px;
+				background: blue;
+				margin: 5px 15px;
+			}
+		}
+	}
+	.pick{
+		background: orange !important;
 	}
 </style>
